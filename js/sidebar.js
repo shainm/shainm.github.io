ce4d562313a32893c16bd1f08854fa998a538724
shainm <54271530+shainm@users.noreply.github.com>
2019-08-23 12:55:58 -0400

    made sidebar button wider

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,13 +1,13 @@
 function openNav() {
 	document.getElementById("sidebar").style.width = "150px";
-	document.getElementById("content").style.marginLeft = "175px";
+	document.getElementById("content").style.marginLeft = "200px";
 	document.getElementById("openbtn").style.marginLeft = "150px";
 	document.getElementById("openbtn").setAttribute("onClick", "closeNav()");
 }
 
 function closeNav() {
 	document.getElementById("sidebar").style.width = "0";
-	document.getElementById("content").style.marginLeft= "25px";
+	document.getElementById("content").style.marginLeft= "50px";
 	document.getElementById("openbtn").style.marginLeft = "0";
 	document.getElementById("openbtn").setAttribute("onClick", "openNav()");
 }
